2c359f82add8b84a25d7470cad99eabd1fd3c966
subash karki <subashkarki68@gmail.com>
2023-08-08 03:25:20 +0545

    first

--- src/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Header() {
-  // Navigation bar or header section
-  return <div className="header">{/* Add navigation elements */}</div>;
-}
-
-export default Header;
